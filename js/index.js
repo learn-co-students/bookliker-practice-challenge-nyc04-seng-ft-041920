41511e7c3ad5d66553ee8a830a2bb72f71fe75c3
residenttrickster <metkusblackus@gmail.com>
2020-06-30 16:50:30 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+
+    //DOM element 
+    const booksCollector = document.querySelector("#list")
+    const oneBook = document.querySelector("#show-panel")
+
+    //Event listeners
+
+    //Render helper 
+    function renderOneBook(bookObject) {
+        oneBook.innerHTML = `<h1>${bookObject.title}</h1><img src="${bookObject.img_url}" alt="${bookObject.title}">
+        <p>${bookObject.description}</p>`
+
+    }
+
+    function renderAllBooks(bookObject) {
+        const bookLi = document.createElement("li")
+        bookLi.className = "item"
+        bookLi.dataset.id = bookObject.id
+        bookLi.textContent = bookObject.title
+
+        booksCollector.append(bookLi)
+
+        bookLi.addEventListener("click", function(event) {
+            renderOneBook(bookObject)
+        })
+
+    }
+
+    //initial render
+
+fetch('http://localhost:3000/books')
+    .then(response => response.json())
+    .then(bookArray => {
+        bookArray.forEach(renderAllBooks)
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+});
